ad83138c47c037bfc6971c6b6ab6d6bfab28aea4
Pauatro <pauatro@gmail.com>
2023-12-03 20:14:52 +0100

    feat: add schemas and endpoint exceptions

---
+++ src/translation/endpoints/schemas.py
@@ -0,0 +1,16 @@
+from enum import Enum;
+from pydantic import BaseModel
+
+class AvailableLanguages(str, Enum):
+    spanish = "spanish"
+    german = "german"
+    italian = "italian"
+    danish = "danish"
+    french = "french"
+    english = "english"
+
+available_languages_list = [l.value for l in AvailableLanguages]
+
+class TranslationRequestSchema(BaseModel):
+    language: AvailableLanguages
+    text: str
