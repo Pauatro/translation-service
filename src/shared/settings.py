f11a7b51b892c613b9184cd9d02d5acafc52f5ad
Pauatro <pauatro@gmail.com>
2023-12-05 16:47:14 +0100

    chore: update cores origins

--- src/shared/settings.py
+++ src/shared/settings.py
@@ -5,7 +5,6 @@ from typing import List, AnyHttpUrl
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [
-        "http://localhost:3000",
-        "http://localhost:8001",  # type: ignore
-        "https://fastapi-recipe-app.herokuapp.com",
+        "http://localhost:5173",
+        "https://translation-service-app.herokuapp.com",
     ]
